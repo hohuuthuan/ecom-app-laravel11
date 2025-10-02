a9b6847a04db2f7f435626cd3ddee7aa87ee14d6
Huu Thuan <hohuuthuan789@gmail.com>
2025-10-02 13:58:36 +0700

    fix(head-user): cofigure vite input css

--- resources/views/partials/user/head.blade.php
+++ resources/views/partials/user/head.blade.php
@@ -6,6 +6,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/toast.css') }}">
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+@vite(['resources/css/toast.css'])
+@vite(['resources/css/style.css'])
+@vite(['resources/css/custom.css'])

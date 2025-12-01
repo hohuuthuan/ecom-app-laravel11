249f2c590509e0d68a82a5bada9f76c13dc221b6
Huu Thuan <hohuuthuan789@gmail.com>
2025-12-01 18:44:15 +0700

    fix(warehouse): make left sidebar fixed and scroll only main content

--- resources/views/layouts/warehouse.blade.php
+++ resources/views/layouts/warehouse.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts.base')
 
 @section('head')
-@include('partials.admin.head')
+  @include('partials.admin.head')
 @endsection
 
 @section('body_class','warehouse-page')
 
 @section('layout')
-
-<div class="container-fluid g-0">
-  <div class="row h-100 g-0 flex-nowrap">
-    <div class="col-auto px-0 warehouse-sidebar">
-      @include('partials.admin.warehouseSidebar')
-    </div>
-    <div class="col p-4 warehouse-main">
-      @yield('content')
-    </div>
+  @include('partials.admin.warehouseSidebar')
+  <div class="warehouse-main">
+    @yield('content')
   </div>
-</div>
 
-
-@include('partials.admin.script')
+  @include('partials.admin.script')
 @endsection

727d99cb7eefe573bc0f16c4d63eb58789ae0cdf
Huu Thuan <hohuuthuan789@gmail.com>
2025-12-11 19:15:55 +0700

    fix sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.base')
 
 @section('head')
-@include('partials.admin.head')
+  @include('partials.admin.head')
 @endsection
 
 @section('body_class','admin-page')
 
 @section('layout')
-
-<div class="container-fluid g-0">
-  <div class="row g-0">
+<div class="container-fluid g-0 admin-layout">
+  <div class="row g-0 admin-layout-row flex-nowrap">
     @include('partials.admin.sidebar')
-    <div id="contentCol" class="col d-flex flex-column min-vh-100">
+
+    <div id="contentCol" class="col d-flex flex-column min-vh-100 admin-content-col">
       @include('partials.admin.header')
+
       <main class="wrapper-content flex-grow-1">
         @yield('content')
       </main>
+
       @include('partials.admin.footer')
     </div>
   </div>
